4fb8dcdd897d18c4b4c29a33c04aa627ab927089
Mihai Potra <mike@mpotra.com>
2021-05-13 15:10:54 +0300

    plugin: Add initial custom plugin

---
+++ _typedoc-export-for-jekyll-plugin/src/index.ts
@@ -0,0 +1,10 @@
+import { Application } from 'typedoc/dist/lib/application';
+import { ParameterType } from 'typedoc/dist/lib/utils/options/declaration';
+
+import { TemplatePlugin } from './plugin';
+
+export = (PluginHost: Application) => {
+  const app = PluginHost.owner;
+
+  app.renderer.addComponent('jekyll-template-export', new TemplatePlugin(app.renderer));
+};
